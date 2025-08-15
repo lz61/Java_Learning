78645529a02aa88a9cd1c3f6931bc760597a1ee8
ampSak <ampsak@ampSakdeMacBook-Air.local>
2025-08-16 07:56:10 +0800

    Local Push

--- code/Student.java
+++ code/Student.java
@@ -2,12 +2,28 @@ public class Student {
     String id;
     String name;
 
-    Student() {
-        id = "Default ID";
-        name = "Default Name";
+    // 默认的构造函数,初始化函数
+    // 不需要返回值,void
+    Student(){
+        id = "垃圾ID";
+        name = "DefaultName";
     }
 
-    void print() {
-        System.out.println("Hello World!");
+    Student(String student_id){
+        id = student_id;
     }
+
+    // "student_id",变量
+    Student(String student_id,String student_name){
+        id = student_id;
+        name = student_name;
+    }
+
+    void chooseCourse(){
+        
+    }
+
+    // 先创建一个Teacher类
+    // 尝试撰写一个关于Teacher的初始化函数,你可以自定义teacher的默认名字和id,
+    // 请在主程序创建一个Teacher类型的变量,并输出其名字和id.
 }
--- code/Test.java
+++ code/Test.java
@@ -1,27 +1,16 @@
 public class Test {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] evenArr = evenNum(arr);
-        for(int i : evenArr){
-            System.out.print(i + " ");
-        }
+        // System.out.println("Hello");  
     }
+    // student; 选课
+    // teacher： 能够开课
 
-    static int[] evenNum(int[] x) {
-        int count = 0;
-        for(int i=0;i<x.length;i++){
-            if(x[i] % 2 == 0){
-                count++;
-            }
-        }
-        int[] even = new int[count];
-        int index = 0;
-        for(int i=0;i<x.length;i++){
-            if(x[i] % 2 == 0){
-                even[index] = x[i];
-                index++;
-            }
-        }
-        return even;
+    //非static函数
+    static String tryPrint(){
+        System.out.println("Hello World!");
+        return "1234";
     }
 }
+
+// Character
+// print
